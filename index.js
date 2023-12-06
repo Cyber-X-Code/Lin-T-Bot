ada67e179a9dfcae85cab7c4528fd1bdd874a6ac
Cyber X <cyber.x@list.ru>
2023-12-06 12:24:35 +0300

    Update index

--- index.js
+++ index.js
@@ -8,24 +8,12 @@ const bot = new Telegraf(token);
 const { part001Composer } = require('./composers/part001.composers');
 const { welcomeMessage } = require('./message');
 
+
 bot.start((ctx) => {
   const image = {
     source: './img/Linux-Logo.png'
   };
-
-  ctx.replyWithPhoto(image, {
-    caption: (welcomeMessage[0]),
-    reply_markup: Markup.inlineKeyboard([
-      Markup.callbackButton('Продолжить', 'part001')
-    ])
-  });
-});
-
-bot.start((ctx) => {
-  const image = {
-    source: './img/igor.jpg'
-  };
-
+  
   const message = {
     caption: (welcomeMessage[0]),
     reply_markup: Markup.inlineKeyboard([
@@ -34,8 +22,56 @@ bot.start((ctx) => {
   };
 
   ctx.replyWithPhoto(image, message);
+
+  // ctx.replyWithPhoto(image, message, {
+    
+  // });
 });
 
 bot.use(part001Composer);
 
 bot.launch();
+
+
+
+// // Файл index.js
+// const { Telegraf, Markup } = require('telegraf');
+// require('dotenv').config()
+
+// const token = process.env.TOKEN
+// const bot = new Telegraf(token);
+
+// const { part001Composer } = require('./composers/part001.composers');
+// const { welcomeMessage } = require('./message');
+
+// bot.start((ctx) => {
+//   const image = {
+//     source: './img/Linux-Logo.png'
+//   };
+
+//   ctx.replyWithPhoto(image, {
+//     caption: (welcomeMessage[0]),
+//     reply_markup: Markup.inlineKeyboard([
+//       Markup.callbackButton('Продолжить', 'part001')
+//     ])
+//   });
+// });
+
+// bot.start((ctx) => {
+//   const image = {
+//     source: './img/igor.jpg'
+//   };
+
+//   const message = {
+//     caption: (welcomeMessage[0]),
+//     reply_markup: Markup.inlineKeyboard([
+//       Markup.callbackButton('Начать', 'part001')
+//     ])
+//   };
+
+//   ctx.replyWithPhoto(image, message);
+// });
+
+// bot.use(part001Composer);
+
+// bot.launch();
